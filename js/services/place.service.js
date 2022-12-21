faf8b074d9c2984746b89bb963992b54b8df13f5
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-21 16:45:52 +0200

    Add place saving functionality

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -1,7 +1,8 @@
 import { storageService } from './async-storage.service.js'
 
 export const placeService = {
-    query
+    query,
+    save
 }
 
 const API_KEY = 'AIzaSyBzYOvWP9Fcn5o3hqah4fNiufkLax8i_Hg'
@@ -9,4 +10,12 @@ const STORAGE_KEY = 'placeDB'
 
 function query() {
     return storageService.query(STORAGE_KEY)
+}
+
+function save(place){
+    if(place.id){
+        return storageService.put(STORAGE_KEY, place)
+    } else {
+        return storageService.post(STORAGE_KEY, place)
+    }
 }
