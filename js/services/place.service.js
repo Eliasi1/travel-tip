86c6d0b3372745c9b3cb06b03976903a7afa6400
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-21 16:55:51 +0200

    Add demo places & util.service

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -1,4 +1,5 @@
 import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
 
 export const placeService = {
     query,
@@ -7,15 +8,46 @@ export const placeService = {
 
 const API_KEY = 'AIzaSyBzYOvWP9Fcn5o3hqah4fNiufkLax8i_Hg'
 const STORAGE_KEY = 'placeDB'
+_createDemoPlaces()
 
 function query() {
     return storageService.query(STORAGE_KEY)
 }
 
-function save(place){
-    if(place.id){
+function save(place) {
+    if (place.id) {
         return storageService.put(STORAGE_KEY, place)
     } else {
         return storageService.post(STORAGE_KEY, place)
     }
+}
+
+function _createDemoPlaces() {
+    const places = [
+        {
+            id: '',
+            name: '',
+            lat: 31.967127,
+            lng: 34.839411,
+            createdAt: Date.now(),
+            updatedAt: null
+        },
+        {
+            id: '',
+            name: '',
+            lat: 31.967127,
+            lng: 34.839411,
+            createdAt: Date.now(),
+            updatedAt: null
+        },
+        {
+            id: '',
+            name: '',
+            lat: 31.967127,
+            lng: 34.839411,
+            createdAt: Date.now(),
+            updatedAt: null
+        }
+    ]
+    utilService.saveToStorage(STORAGE_KEY, places)
 }
